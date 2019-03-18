736e295b5f51b32a67962257fa6459af4835d90c
avbabushkin <alexbub72@gmail.com>
2019-03-18 14:10:54 +0300

    schema save

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_092109) do
+ActiveRecord::Schema.define(version: 2019_02_09_195443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  # create_table "articles", force: :cascade do |t|
-  #   t.string "title"
-  #   t.string "text"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
 
   create_table "dreams", force: :cascade do |t|
     t.string "dream_title"
