362c1329193cba8a5c4af0e67aff289b3a2e7ee6
avbabushkin <alexbub72@gmail.com>
2019-03-21 16:45:10 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  resources :dreams, except: [:index, :show, :new]
+  resources :dreams 
+  
   root 'static_pages#home'
   match '/signup',  to: 'users#new', via: 'get'
   match '/signin',  to: 'sessions#new', via: 'get'
