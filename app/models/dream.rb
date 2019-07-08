275320bb0674fdf72da39266fcc0da7943f8d319
avbabushkin <alexbub72@gmail.com>
2019-07-08 10:41:17 +0300

    delete old model Dream

--- app/models/dream.rb
+++
@@ -1,6 +0,0 @@
-class Dream < ApplicationRecord
-  validates :dream_title, :dream_text, presence: true
-  validates :dream_title, length: { in: 2..50 }
-  validates :dream_text, length: { maximum: 500,
-    too_long: "Слишком длинный текст, допустимо %{count} символов" }
-end
