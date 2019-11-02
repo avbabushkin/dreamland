7874dc1f32999bc027ed9d10c251708e0f708680
avbabushkin <alexbub72@gmail.com>
2019-11-02 18:35:21 +0300

    Refactoring: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
-  get 'login'   => 'sessions#new'
-  get 'login'   => 'sessions#create'
-  get 'logout'  => 'sessions#destroy'
+  get     'about'     => 'static_pages#about'
+  get     'contact'   => 'static_pages#contact'
+  get     'signup'    => 'users#new'
+  get     'login'     => 'sessions#new'
+  post    'login'     => 'sessions#create'
+  delete  'logout'    => 'sessions#destroy'
 
   resources :users #, except: [:index]
   resources :dreams 
-  resources :sessions, only: [:new, :create, :destroy]
 end
