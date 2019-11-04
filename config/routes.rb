08881a002df9efc882401070b7c5515474014d04
avbabushkin <alexbub72@gmail.com>
2019-11-04 03:46:50 +0300

    Change routes & show user view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   post    'login'     => 'sessions#create'
   delete  'logout'    => 'sessions#destroy'
 
-  resources :users #, except: [:index]
+  resources :users, except: [:index, :destroy]
   resources :dreams 
 end
