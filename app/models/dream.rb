d4fd3b1ea4333f05717e0fdeaf3907715e7d2ebb
avbabushkin <alexbub72@gmail.com>
2019-11-19 09:37:53 +0300

    Refactoring models with rubocop

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Dream < ApplicationRecord
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  VALID_EMAL_REGEX = /.+@.+\..+/i
+  VALID_EMAL_REGEX = /.+@.+\..+/i.freeze
 
   has_many :dreams, dependent: :destroy
-  
-  validates :email, presence: true, 
+
+  validates :email, presence: true,
                     uniqueness: true,
                     format: { with: VALID_EMAL_REGEX }
-  validates :password, length: { in: 6..20 }  
+  validates :password, length: { in: 6..20 }
 
   has_secure_password
 end
